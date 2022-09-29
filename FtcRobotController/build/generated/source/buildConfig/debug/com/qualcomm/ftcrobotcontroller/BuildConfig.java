4cbaeb699120288237dc16a514a85cfd5066ebec
lucasli2 <100967310+lucasli2@users.noreply.github.com>
2022-09-29 15:22:10 -0700

    with vuforia now

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-09-27T16:22:30.443-0700";
+  public static final String APP_BUILD_TIME = "2022-09-28T16:52:17.238-0700";
 }
