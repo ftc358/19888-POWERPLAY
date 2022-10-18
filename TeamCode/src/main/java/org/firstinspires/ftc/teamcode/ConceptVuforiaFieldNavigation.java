3c595a4884e6b05f14d06caa7ffcb08c49f5c9bb
lucasli2 <100967310+lucasli2@users.noreply.github.com>
2022-10-18 15:57:35 -0700

    Cleaned up. Has TFOD and Vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaFieldNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaFieldNavigation.java
@@ -74,7 +74,7 @@ import java.util.List;
  */
 
 @TeleOp(name="Vuforia Field Nav", group ="Concept")
-@Disabled
+
 public class ConceptVuforiaFieldNavigation extends LinearOpMode {
 
     // IMPORTANT:  For Phone Camera, set 1) the camera source and 2) the orientation, based on how your phone is mounted:
