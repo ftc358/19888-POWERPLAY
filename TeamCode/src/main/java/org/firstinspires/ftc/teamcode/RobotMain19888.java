52aaf6a6dce34ffa562ed7675cc0bbd0e4979845
lucasli2 <100967310+lucasli2@users.noreply.github.com>
2022-11-14 15:33:42 -0800

    Update RobotMain19888.java
    version 11/14/22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMain19888.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMain19888.java
@@ -2,113 +2,235 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class RobotMain19888 extends LinearOpMode {
-    final double DRIVE_FACTOR = 1.0;
+    final double DRIVE_FACTOR = 1000.0 / 19.0; //19 inches = 1000 ticks
+    final double TURN_FACTOR = 950.0 / 180.0; //180 degrees = 950 ticks
+    final double STRAFE_FACTOR = 1.0;
 
-    protected DcMotor m1;
-    protected DcMotor m2;
-    protected DcMotor m3;
-    protected DcMotor m4;
+    protected DcMotor lf, lr, rf, rr;
+    protected DcMotor mid;
+    protected DcMotor slideLeft, slideRight;
+    protected Servo leftHand = null;
+    protected Servo rightHand = null;
+
+    public int AutoDir[] = {1, 1, 1, 1};
 
     public void ROBOT_INITIALIZE() throws InterruptedException {
-        m1 = hardwareMap.dcMotor.get("motor1");
-        m2 = hardwareMap.dcMotor.get("motor2");
-        m3 = hardwareMap.dcMotor.get("motor3");
-        m4 = hardwareMap.dcMotor.get("motor4");
+        lf = hardwareMap.dcMotor.get("lf");
+        lr = hardwareMap.dcMotor.get("lr");
+        rf = hardwareMap.dcMotor.get("rf");
+        rr = hardwareMap.dcMotor.get("rr");
+        mid = hardwareMap.dcMotor.get("mid");
 
-        m1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        m2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        m3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        m4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideLeft = hardwareMap.dcMotor.get("leftSlide");
+        slideRight = hardwareMap.dcMotor.get("rightSlide");
+        leftHand = hardwareMap.get(Servo.class, "left_hand");
+        rightHand = hardwareMap.get(Servo.class, "right_hand");
 
-        m1.setDirection(DcMotor.Direction.REVERSE);
-        m2.setDirection(DcMotor.Direction.REVERSE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mid.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        lr.setDirection(DcMotor.Direction.REVERSE);
+        slideRight.setDirection(DcMotor.Direction.REVERSE);
+
+//        leftHand.setPosition(0);
+//        rightHand.setPosition(0);
+
+
+
+
+
     }
 
-    public void MoveV(double inch, double power) {
+
+
+
+    /*============================Autonomous Code================================*/
+
+
+
+
+    public void MoveStraight(double inch, double power) {
+        for (int i = 0; i < 4; i++) {AutoDir[i] = -1;}
         int ticks = (int) (inch * DRIVE_FACTOR);
+        Move(ticks, power);
+    }
 
-        m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void Strafe(double inch, double power) {
 
-        m1.setTargetPosition(m1.getCurrentPosition() - ticks);
-        m3.setTargetPosition(m3.getCurrentPosition() - ticks);
+        int ticks = (int) (inch * STRAFE_FACTOR);
 
-        m1.setPower(power);
-        m3.setPower(power);
+        mid.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        m1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        m3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        mid.setTargetPosition(mid.getCurrentPosition() - ticks);
 
-        while (m1.isBusy() && m2.isBusy()) {
-            telemetry.addData("m1", -m1.getCurrentPosition());
-            telemetry.addData("m3", -m3.getCurrentPosition());
+        mid.setPower(power);
+
+        mid.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (mid.isBusy()) {
+            telemetry.addData("mid", mid.getCurrentPosition());
             telemetry.update();
         }
 
         sleep(200);
     }
 
-    public void MoveH(double inch, double power) {
-        int ticks = (int) (inch * DRIVE_FACTOR);
+    public void Turn(int degrees, double power) {
+        //turning left is positive degrees
+        AutoDir[2] = AutoDir[3] = -1;
+        AutoDir[0] = AutoDir[1] =  1;
+        int ticks = (int) (degrees * TURN_FACTOR);
+        Move(ticks, power);
+    }
 
-        m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        m4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        m2.setTargetPosition(m1.getCurrentPosition() - ticks);
-        m4.setTargetPosition(m3.getCurrentPosition() - ticks);
 
-        m2.setPower(power);
-        m4.setPower(power);
+    public void Move(int ticks, double power) {
 
-        m2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        m4.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        while (m2.isBusy() && m4.isBusy()) {
-            telemetry.addData("m2", -m2.getCurrentPosition());
-            telemetry.addData("m4", -m4.getCurrentPosition());
+        lf.setTargetPosition(lf.getCurrentPosition() - (ticks * AutoDir[0]));
+        lr.setTargetPosition(lr.getCurrentPosition() - (ticks * AutoDir[1]));
+        rf.setTargetPosition(rf.getCurrentPosition() - (ticks * AutoDir[2]));
+        rr.setTargetPosition(rr.getCurrentPosition() - (ticks * AutoDir[3]));
+
+        lf.setPower(power);
+        lr.setPower(power);
+        rf.setPower(power);
+        rr.setPower(power);
+
+        lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (lf.isBusy() && lr.isBusy() && rf.isBusy() && rr.isBusy()) {
+            telemetry.addData("lf", lf.getCurrentPosition());
+            telemetry.addData("lr", lr.getCurrentPosition());
+            telemetry.addData("rf", rf.getCurrentPosition());
+            telemetry.addData("rr", rr.getCurrentPosition());
             telemetry.update();
         }
 
         sleep(200);
     }
 
-    /*--------------Code for mecanum wheel---------------*/
+    public void SlideOP(int direction, double power){
+        int ticks = 69 * direction;
+        slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-    public void TeleStraight() {
-        m1.setPower(gamepad2.left_stick_y);
-        m2.setPower(gamepad2.left_stick_y);
-        m3.setPower(gamepad2.left_stick_y);
-        m4.setPower(gamepad2.left_stick_y);
+        slideLeft.setTargetPosition(slideLeft.getCurrentPosition()-(ticks));
+        slideRight.setTargetPosition(slideRight.getCurrentPosition()-(ticks));
+        slideLeft.setPower(power);
+        slideRight.setPower(power);
+        while (slideLeft.isBusy() && slideRight.isBusy()) {
+            telemetry.addData("slideLeft", slideLeft.getCurrentPosition());
+            telemetry.addData("slideRight", slideRight.getCurrentPosition());
+            telemetry.update();
+        }
     }
 
+
+    /*============================TeleOp Code================================*/
+
+
+    public double powerfactor(){
+        if (gamepad1.left_bumper){
+            return 1;
+        }
+        else{
+            return 0.5;
+        }
+    }
+
+    public boolean TeleStraight() {
+        if (gamepad1.left_stick_y > 0.25 || gamepad1.left_stick_y < -0.25) {
+            //right now left stick x = 0
+            //if it wants to turn left it will become -1
+            //right, +1
+            double leftWheels, rightWheels;
+
+            if (gamepad1.left_stick_x<-0.1){
+                leftWheels = gamepad1.left_stick_y * (1+gamepad1.left_stick_x);
+                rightWheels = (gamepad1.left_stick_y);
+            }//for left turns
+            else if (gamepad1.left_stick_x>0.1){
+                leftWheels = (gamepad1.left_stick_y);
+                rightWheels = gamepad1.left_stick_y * (1-gamepad1.left_stick_x);
+            }//for right turns
+            else{
+                leftWheels = (gamepad1.left_stick_y);
+                rightWheels = (gamepad1.left_stick_y);
+            }
+
+            lf.setPower(-leftWheels*powerfactor());
+            lr.setPower(-leftWheels*powerfactor());
+            rf.setPower(-rightWheels*powerfactor());
+            rr.setPower(-rightWheels*powerfactor());
+            return true;
+        }
+        return false;
+    }
     public void TeleStrafe() {
-        m1.setPower(gamepad2.left_stick_x);
-        m2.setPower(gamepad2.left_stick_x);
-        m3.setPower(-gamepad2.left_stick_x);
-        m4.setPower(-gamepad1.left_stick_x);
-    }
-
-    public void TeleDiagonal() {
-        double x = gamepad2.right_stick_x, y = gamepad2.right_stick_y;
-        if (x < 0) {
-            m2.setPower(y);
-            m4.setPower(y);
-        }else {
-            m1.setPower(y);
-            m3.setPower(y);
+        if (gamepad1.right_stick_x > 0.25 || gamepad1.right_stick_x < -0.25) {
+            mid.setPower(gamepad1.right_stick_x);
+        }
+        else {
+            mid.setPower(0);
         }
     }
 
     public void TeleTurn() {
-        int dir = 0;
-        if (gamepad2.dpad_right) dir = 1;
-        if (gamepad2.dpad_left) dir = -1;
-        m1.setPower(0.8 * dir);
-        m2.setPower(0.8 * dir);
-        m3.setPower(-0.8 * dir);
-        m4.setPower(-0.8 * dir);
+        double power = gamepad1.left_stick_x * powerfactor();
+        lf.setPower(power);
+        lr.setPower(power);
+        rf.setPower(-power);
+        rr.setPower(-power);
     }
+
+    public void TeleSlide() {
+        if (gamepad1.right_bumper) {
+            slideLeft.setPower(1);
+            slideRight.setPower(1);
+        }
+        if (gamepad1.a){
+            slideLeft.setPower(-1);
+            slideRight.setPower(-1);
+
+        }
+        else{
+            slideLeft.setPower(0);
+            slideRight.setPower(0);
+        }
+    }
+
+    public void TeleClaw() {
+        //position 0 is the grab function
+        // position 1 or -1 is the release?
+
+        //make a toggle
+        if (gamepad1.x){
+            leftHand.setPosition(0.6);
+            rightHand.setPosition(0.4);
+        }
+        if (gamepad1.b){
+            leftHand.setPosition(0.4);
+            rightHand.setPosition(0.6);
+        }
+    }
+    
 
 }
