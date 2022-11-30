9b07a91900c026fb286a3c25049ea05ef5f1f7ca
DayunSuh <73674333+DayunSuh@users.noreply.github.com>
2022-11-29 16:09:12 -0800

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain19888BLUEconePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMain19888BLUEconePark.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
-public class AutoMain19888coneANDpark extends RobotMain19888 {
+public class AutoMain19888BLUEconePark extends RobotMain19888 {
     private boolean done = false;
     public void runOpMode() throws InterruptedException {
         ROBOT_INITIALIZE();
@@ -12,15 +12,20 @@ public class AutoMain19888coneANDpark extends RobotMain19888 {
             double power_value=0.5;
             //Cone
             MoveStraight(29, power_value);
-            Turn(45, power_value);
+            Turn(-45, power_value);
             Thread.sleep(500);
             MoveStraight(9, power_value); //the exact length calculated = 9.09556
-            //slideOp(); -> after function is completed
+            SlideOP(1, 0.7);
             //claw???(); -> after function is completed
-            
             //Park
 
             done = true;
+        }
+
+        if (done){
+            telemetry.addData("###############", null);
+            telemetry.addData("Mission CMPL", null);
+            telemetry.addData("###############", null);
         }
     }
 }
